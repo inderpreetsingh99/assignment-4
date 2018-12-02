399cd83723c4c43bfac6e2d2fe42b91f62486c4e
inderpreetsingh99 <43333262+inderpreetsingh99@users.noreply.github.com>
2018-12-02 23:33:17 +0530

    Add files via upload

---
+++ A3INDERPREETSINGHP1/A3INDERPREETSINGHP1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3INDERPREETSINGHP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+        again:
+            Console.WriteLine("MENU:");
+            Console.WriteLine("1.display list of even numbers beginning at 0");
+            Console.WriteLine("2.display sequence of perfect squares");
+            Console.WriteLine("3.exit");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("DISPLAY EVEN MUMERALS");
+                    string n = Console.ReadLine();
+                    int num = int.Parse(n);
+                    for (int i = 0; i <= num; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    break;
+                case "2":
+                    int x = 1;
+                    while (x >= 0)
+                    {
+                        int ans = x * x;
+                        Console.WriteLine(ans);
+                        Console.WriteLine("PRESS Z TO DISPLAY MENU");
+                        string answer = Console.ReadLine();
+                        if (answer == "Z")
+                        {
+                            x++;
+                            Console.WriteLine(ans);
+                        }
+                        else
+                        {
+                            goto again;
+                        }
+                    }
+                    break;
+                case "3": break;
+
+                default: goto again;
+            }
+        }
+    }
+}
---
+++ A3INDERPREETSINGHP2/A3INDERPREETSINGHP2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3INDERPREETSINGHP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int scndNum;
+            string operation;
+            int result;
+            try
+            {
+            again:
+                Console.Write("ENTER NOMBRE UN (FRENCH) : ");
+                firstNum = Convert.ToInt32(Console.ReadLine());
+                Console.Write("ENTER NOMBRE DEUX(FRENCH): ");
+                scndNum = Convert.ToInt32(Console.ReadLine());
+            num:
+                Console.Write("enter expressions for addition subtraction multiplication & division ");
+                operation = Console.ReadLine();
+                if (operation == "multiplication")
+                {
+                    result = firstNum * scndNum;
+                    Console.WriteLine(firstNum + "x" + scndNum + "=" + result);
+                    goto again;
+                }
+                else if (operation == "division")
+                {
+                    result = firstNum / scndNum;
+                    Console.WriteLine(firstNum + "/" + scndNum + "=" + result);
+                    goto again;
+                }
+                else if (operation == "addition")
+                {
+                    result = firstNum + scndNum;
+                    Console.WriteLine(firstNum + "+" + scndNum + "=" + result);
+                    goto again;
+                }
+                if (operation == "subraction")
+                {
+                    result = firstNum - scndNum;
+                    Console.WriteLine(firstNum + "-" + scndNum + "=" + result);
+                    goto again;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid operation!!!");
+                    goto num;
+                }
+
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Enter a valid arithmetic expression!!!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter a valid format!!!");
+            }
+        }
+    }
+}
